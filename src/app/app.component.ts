144d0b58ebcd996c6674664b0fb1f386f85525a6
Shibnath Salui <shibnath96@gmail.com>
2018-08-24 11:34:22 +0530

    Saved on [24.08.2018] at 11:36 AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pacman-game-demo';
+export class AppComponent implements OnInit{
+  
+  title = 'Pacman Game';
+
+  constructor() {
+    document.title = this.title;
+  }
+
+  ngOnInit() {}
+
 }
